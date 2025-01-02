ecdcbffef82eff74874c156580a0996d489b75b0
Snakejuice87 <117030692+Snakejuice87@users.noreply.github.com>
2025-01-03 06:33:51 +0800

    Update Sankakucomplex-ad-block-click.js

--- mine/Sankakucomplex-ad-block-click.js
+++ mine/Sankakucomplex-ad-block-click.js
@@ -3,8 +3,23 @@
 // @description    code to press continue button
 // @match      https://chan.sankakucomplex.com/*
 // @match      https://chan.sankakucomplex.com/*/*
+// @match       *://chan.sankakucomplex.com
+// @match       *://idol.sankakucomplex.com/*
+// @match       *://legacy.sankakucomplex.com/*
 // @version       1.0
 // ==/UserScript==
+
+(function() {
+    'use strict';
+    var button = document.querySelector('button[name="button"][class="center secondary"]');
+
+    if (button) {
+        button.click();
+        console.log("adblock skipped warning. Now you will no longer be redirected to the page https://chan.sankakucomplex.com/static/disable_adblock");
+    } else {
+        console.log('Button not found');
+    }
+})();
 
 (function() {
     'use strict';
