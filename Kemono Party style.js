11e501281cf01ce26eeca5a887f2fde6e3193466
Snakejuice87 <117030692+Snakejuice87@users.noreply.github.com>
2025-01-07 21:55:03 +0800

    Update Kemono Party style.js

--- Kemono Party style.js
+++ Kemono Party style.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name           Kemono Party style
 // @description    custom css style for kemono party
-// @include        https://kemono.party/#*
-// @include        https://kemono.party/*/user/*/
+// @include        https://kemono.su/*
+// @include        https://kemono.su/*/user/*/
 // @run-at         document-start
 
 // ==/UserScript==
@@ -10,7 +10,7 @@
 // NEW SITES CAN BE ADDED NOW
 
 var css =
-		"*:visited { color: orange; border: blue 2px solid !important; } card-list__items, div:visited { border: blue 2px solid !important; } card-list__items:visited { border: blue 2px solid !important; } a, card-list__items:visited { border: blue 2px solid !important; } card-list__items, div { --card-size: 250px; } post-card__image { objectfit: contain; background: contain; fit: contain; } post-card__image, img { objectfit: contain; background: contain; fit: contain; }",
+		"*:visited { color: orange; border: blue 2px solid !important; } card-list__items, div:visited { border: blue 2px solid !important; } card-list__items:visited { border: blue 2px solid !important; } a, card-list__items:visited { border: blue 2px solid !important; } /* card-list__items, div { --card-size: 250px; } post-card__image { objectfit: contain; background: contain; fit: contain; } post-card__image, img { objectfit: contain; background: contain; fit: contain; } */",
 	head = document.head || document.getElementsByTagName("head")[0],
 	style = document.createElement("style");
 
